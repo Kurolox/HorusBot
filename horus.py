5b2dd0bff582f2dad3a9eb59dd1193d2a5821112
Carlos Gomez <kurolox@gmail.com>
2017-03-19 17:31:57 +0100

    Refactoring code. Still WIP.

--- horus.py
+++ horus.py
@@ -20,16 +20,36 @@ async def on_message(msg):
     if msg.content.startswith('!test'):
         await bot_client.send_message(msg.channel, 'Test received!')
     elif msg.content.startswith('<@291477047962763264>'):
-        try: 
-            syntax_language = str(msg.content.split(' ')[1])
-        except IndexError:
-            await bot_client.send_message(msg.channel, "You need to specify the syntax language if you want me to run it!")
-        print(syntax_language)
-        print(msg.content.split('```'))
-        
+        code = check_code(msg.content)
+        if not code:
+            await bot_client.send_message(msg.channel, "There's no code to run.")
+        else:
+            print(code)
+            lang = check_lang(msg.content)
+            if not lang:
+                await bot_client.send_message(msg.channel, "The language wasn't specified.")
+            else:
+                # TODO: Run code
+                return 0
         #TODO: Research how to sandbox a python program
 
-        await bot_client.send_message(msg.channel,'```%s```' % msg.content.split('```')[1])
+
+def check_code(message):
+    try:
+        primitive_code = message.split("```")[1]
+    except IndexError:
+        return 0
+    print(primitive_code)
+    return primitive_code.split("\\n")
 
 
-bot_client.run('')
+
+
+def check_lang(message):
+    return 0
+    # TODO: Split message into args and syntax markdown
+
+    # TODO: Check if it's using one of them, or both at the same time
+
+    # TODO: Return the lang
+
