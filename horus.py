61eb597c5a7a56dbc2698658a5e0e740c78eabc3
Carlos Gomez <kurolox@gmail.com>
2017-03-20 12:15:22 +0100

    Added part of lang checking

--- horus.py
+++ horus.py
@@ -18,15 +18,15 @@ async def on_ready():
 @bot_client.event
 async def on_message(msg):
     print('MSG: ' + msg.content)
-    if msg.content.startswith('!test'):
+    if msg.content.startswith('!test'):  # Test function
         await bot_client.send_message(msg.channel, 'Test received!')
-    elif msg.content.startswith('<@291477047962763264>'):
+    elif msg.content.startswith('<@291477047962763264>'): # Toggled when the bot is mentioned
         code = check_code(msg.content)
         if not code:
             await bot_client.send_message(msg.channel, "There's no code to run.")
         else:
             print(code)
-            lang = check_lang(msg.content)
+            lang = check_lang(msg.content, code)
             if not lang:
                 await bot_client.send_message(msg.channel, "The language wasn't specified.")
             else:
@@ -36,17 +36,28 @@ async def on_message(msg):
 
 
 def check_code(message):
+    '''Checks if the message actually does have code to run'''
     try:
         primitive_code = message.split("```")[1]
     except IndexError:
         return 0
-    print(primitive_code)
-    return primitive_code.split("\\n")
+    return primitive_code.split("\n")
 
 
-
-
-def check_lang(message):
+def check_lang(message, code):
+    '''Checks if there's an argument or a markdown language specified in the code.'''
+    compatible_languages = {
+            "Python" : ("python", "py", "pycode"),
+            "test" : ("test")}
+    try: 
+        argument = message.split("```")[0].split(" ")[1] # Checks argument
+    except IndexError:
+        argument = ""
+    for language, diff_naming in compatible_languages.items():
+        if argument in diff_naming:
+            print(language)
+        else:
+            
     return 0
     # TODO: Split message into args and syntax markdown
 
