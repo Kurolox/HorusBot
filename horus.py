9e9ec3ea619c75b8eae979d672765362943835cb
Carlos Gomez <kurolox@gmail.com>
2017-03-22 10:13:01 +0100

    Improved readability. Removed some debugging prints.

--- horus.py
+++ horus.py
@@ -27,30 +27,11 @@ async def on_message(msg):
     if msg.content.startswith('!test'):  # Test function
         await bot_client.send_message(msg.channel, 'Test received!')
     elif msg.content.startswith('<@291477047962763264>'):  # Toggled when the bot is mentioned
-        code = check_code(msg.content)
-        if not code:
-            await bot_client.send_message(msg.channel, "There's no code to run.")
-        else:
-            lang, code = check_lang(msg.content, code)
-            if not lang:
-                await bot_client.send_message(msg.channel, "The language wasn't specified.")
-            else:
-                filename = build_exec(lang, code)
-                print(filename)
-                output, error, loop_flag = run_code(lang, filename)
-                if error:
-                    await bot_client.send_message(msg.channel, "The bot has encountered the following error when running your %s code. \n```\n%s```" % (lang, error))
-                elif loop_flag:
-                    await bot_client.send_message(msg.channel, "I'm sorry, but your request took too long. Here are the last 5 lines from the output.\n```\n%s```" % output)
-                else:
-                    await bot_client.send_message(msg.channel, "Here's the output of your %s code. \n```\n%s```" % (lang, output))
-
-                # TODO: Run code
-        # TODO: Research how to sandbox a python program
+        await code(msg)
 
 
 def check_code(message):
-    '''Checks if the message actually does have code to run'''
+    """Checks if the message actually does have code to run"""
     try:
         primitive_code = message.split("```")[1]
     except IndexError:
@@ -59,7 +40,7 @@ def check_code(message):
 
 
 def check_lang(message, code):
-    '''Checks if there's an argument or a language specified in the code.'''
+    """Checks if there's an argument or a language specified in the code."""
     compatible_languages = {
         "Python": ("python", "py", "pycode")}
     detected_lang = ""
@@ -98,23 +79,48 @@ def build_exec(lang, code):
 
 
 def run_code(lang, filename):
-    # Still WIP.
+    """Runs a file and returns the stdout, stderr and a flag if the process timed out."""
     loop_flag = 0
     if lang == "Python":
         process = Popen(["python", filename], stdout=PIPE, stderr=PIPE)
         try:
-            stdout, stderr = process.communicate(timeout=6)
+            stdout, stderr = process.communicate(timeout=5)
         except TimeoutExpired:
-            line_number = 5
-            stderr = b""
-            stdout = b""
-            while line_number > 0:
-                stdout += process.stdout.readline()
-                print(stdout)
-                line_number -= 1
-            loop_flag = 1
-            # TODO: Make it return the last 5 lines of output instead.
+            stdout, stderr, loop_flag = timeout(process)
         return stdout.decode("utf-8"), stderr.decode("utf-8"), loop_flag
+
+
+def timeout(process):
+    """If the process timed out, returns the last 5 lines of output and kills the process"""
+    line_number = 0
+    stderr = b""
+    stdout = b""
+    while line_number < 6:
+        stdout += process.stdout.readline()
+        line_number += 1
+    loop_flag = 1
+    # TODO: Kill the process.
+    return stdout, stderr, loop_flag
+
+
+async def code(msg):
+    """Grabs the code from a message, identifies the language, runs it and sends the output."""
+    code = check_code(msg.content)
+    if not code:
+        await bot_client.send_message(msg.channel, "There's no code to run.")
+    else:
+        lang, code = check_lang(msg.content, code)
+        if not lang:
+            await bot_client.send_message(msg.channel, "The language wasn't specified.")
+        else:
+            filename = build_exec(lang, code)
+            output, error, loop_flag = run_code(lang, filename)
+            if error:
+                await bot_client.send_message(msg.channel, "The bot has encountered the following error when running your %s code. \n```\n%s```" % (lang, error))
+            elif loop_flag:
+                await bot_client.send_message(msg.channel, "I'm sorry, but your request took too long. Here are the last 5 lines from the output.\n```\n%s```" % output)
+            else:
+                await bot_client.send_message(msg.channel, "Here's the output of your %s code. \n```\n%s```" % (lang, output))
 
 
 bot_client.run(token)
